b3842f34c792c2df6a549fc4a225aca55ac1c779
Jesse Caulfield <jesse.caulfield@keybridgeglobal.com>
2017-01-12 21:23:30 -0500

    prevent double slash when building paths

--- src/main/java/net/java/dev/wadl/Resource.java
+++ src/main/java/net/java/dev/wadl/Resource.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (C) 2017 Key Bridge LLC
  *
  * This program is free software: you can redistribute it and/or modify
@@ -239,7 +239,9 @@ public class Resource implements PathProvider {
    */
   @Override
   public String buildPath() {
-    return parent != null ? parent.buildPath() + "/" + path : path;
+    return parent != null
+           ? parent.buildPath() + (path.isEmpty() ? "" : "/" + path) // avoid double slash
+           : path;
   }
 
   /**
